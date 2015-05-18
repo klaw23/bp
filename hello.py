d8110036cc4f6d863490a1cf2486a74cb3e6a595
Kevin Law <kevin@cambly.com>
2015-05-17 17:38:47 -0700

    Init postmates.

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 import os
+
 from flask import Flask
+from postmates.postmates import PostmatesAPI
 
 app = Flask(__name__)
+
+pm = PostmatesAPI('dc937388-2da1-4652-b539-e77b45d478e2', 'cus_It8IMxcQUK4_ik')
 
 @app.route('/')
 def hello():
